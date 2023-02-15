9b47472a4fc04eda9cab7c7b8cc2131c1f5128f5
Navid Anjum <navids92@gmail.com>
2023-02-15 15:56:44 +0600

    Design Improvement->index page

--- views/index.view.php
+++ views/index.view.php
@@ -1,54 +1,56 @@
 <?php view('/partials/head.php') ?>
-
+<style>
+    #wrapper {
+        display: block;
+    }
+</style>
 
 <section class="vh-100">
     <div class="container-fluid h-custom">
         <div class="row d-flex justify-content-center align-items-center h-100">
-            <div class="col-md-9 col-lg-6 col-xl-5">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-6 mb-5">
 
-                <img src="./views/img/aw_noman.jpg" class="img-fluid" alt="...">
+                        <!-- Registration-->
+                        <div class="card shadow p-5 animated zoomIn slow"  style="height: 33rem">
+                            <img src="./views/img/aw_noman.jpg" class="img-fluid" height="33rem">
 
-            </div>
-            <div class="col-md-8 col-lg-6 col-xl-4 offset-xl-1">
-                <form action="/php_test/login" method="post">
-
-                    <div class="divider d-flex align-items-center my-4">
-                        <p class="text-center fw-bold mx-3 mb-0">
-                            <?php echo $message;?>
-                        </p>
+                        </div>
                     </div>
+                    <div class="col-md-6 mb-5">
 
-                    <!-- Email input -->
-                    <div class="form-outline mb-4">
-                        <input type="text" id="user_id" name="user_id" class="form-control form-control-lg"
-                               placeholder="Enter User ID" />
-                        <label class="form-label" for="form3Example3">Email address</label>
-                    </div>
+                        <div class="card shadow animated zoomIn slow p-5"  style="height: 33rem">
+                            <h3 class="text-center font-weight-bold text-uppercase mb-3">Login Here</h3>
 
-                    <!-- Password input -->
-                    <div class="form-outline mb-3">
-                        <input type="password" id="password" name="password" class="form-control form-control-lg"
-                               placeholder="Enter password" />
-                        <label class="form-label" for="form3Example4">Password</label>
-                    </div>
+                            <form action="/php_test/login" method="post">
+                                <div class="form-group">
+                                    <label>Enter Username</label>
+                                    <input type="text" id="user_id" name="user_id"
+                                           class="form-control form-control-lg"
+                                           />
+                                </div>
+                                <div class="form-group">
+                                    <label>Enter Password</label>
+                                    <input type="password" name="password" class="form-control">
+                                </div>
 
-                    <div class="d-flex justify-content-between align-items-center">
-                        <!-- Checkbox -->
-                        <div class="form-check mb-0">
-                            <input class="form-check-input me-2" type="checkbox" value="" id="form2Example3" />
-                            <label class="form-check-label" for="form2Example3">
-                                Remember me
-                            </label>
+                                <button type="submit" class="btn btn-outline-dark btn-block rounded-pill">Login</button>
+                            </form>
+                            <h6 class="mt-3">Don't have an account? <a href="#"> Create Account Here</a></h6>
+                            <p class="text-center mt-3"> or Login with<p>
+                            <div class="text-center">
+                                <i class="fab fa-facebook mx-2 fa-2x"></i>
+                                <i class="fab fa-twitter  mx-2 fa-2x"></i>
+                                <i class="fab fa-instagram  mx-2 fa-2x"></i>
+                                <i class="fab fa-google  mx-2 fa-2x"></i>
+                            </div>
+
                         </div>
                     </div>
 
-                    <div class="text-center text-lg-start mt-4 pt-2">
-                        <button type="submit" class="btn btn-primary btn-lg"
-                                style="padding-left: 2.5rem; padding-right: 2.5rem;">Login</button>
+                </div>
 
-                    </div>
-
-                </form>
             </div>
         </div>
     </div>
