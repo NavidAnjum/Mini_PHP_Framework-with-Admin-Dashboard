bcaa475461f462c756c390f46f38ecb811b2ee10
Navid Anjum <navids92@gmail.com>
2023-02-15 14:26:35 +0600

    Validate the HTTP method

--- index.php
+++ index.php
@@ -20,5 +20,12 @@
     $uri = parse_url($_SERVER['REQUEST_URI'])['path'];
 
     $method = $_POST['_method'] ?? $_SERVER['REQUEST_METHOD'];
+	// Validate the HTTP method
+	$valid_methods = ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'];
+
+	if (!in_array($method, $valid_methods)) {
+		header('HTTP/1.1 405 Method Not Allowed');
+		exit;
+	}
 
     $router->route($uri, $method);
--- routes.php
+++ routes.php
@@ -5,3 +5,5 @@ $router->get('/php_test/', 'controllers/index.php');
 $router->post('/php_test/login', 'controllers/login.php');
 
 $router->get('/php_test/dashboard', 'controllers/dashboard.php');
+
+$router->get('/php_test/logout', 'controllers/logout.php');
